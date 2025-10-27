9098ab135afd01964d80e472a35f52d1710cfbea
EvalAI Team <evalai@example.com>
2025-10-27 17:29:49 +0100

    Fix: Add rest_framework.authtoken to INSTALLED_APPS for dj-rest-auth

--- backend_django/config/settings.py
+++ backend_django/config/settings.py
@@ -54,6 +54,7 @@ INSTALLED_APPS = [
     
     # Third party
     'rest_framework',
+    'rest_framework.authtoken',
     'rest_framework_simplejwt',
     'rest_framework_simplejwt.token_blacklist',
     'corsheaders',
